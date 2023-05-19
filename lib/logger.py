f693d20f53f5a813cb14887475cc8dd77ddf45af
trent bullard <trent.bullard@gmail.com>
2023-05-19 15:35:19 -0500

    fix logger

--- lib/logger.py
+++ lib/logger.py
@@ -1,14 +1,16 @@
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s] (%(levelname)s) - %(message)s',
-    datefmt='%Y-%m-%dT%H:%M:%S'
-)
-
 logger = logging.getLogger('chatbot')
+logger.setLevel(logging.DEBUG)
 
-handler = logging.FileHandler('logs/chatbot.log')
-handler.setLevel(logging.DEBUG)
-handler.setFormatter(logging.Formatter('[%(asctime)s] (%(levelname)s) - %(message)s'))
-logger.addHandler(handler)
+# File handler
+file_handler = logging.FileHandler('logs/chatbot.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter('[%(asctime)s] (%(levelname)s) - %(message)s'))
+logger.addHandler(file_handler)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter('[%(asctime)s] (%(levelname)s) - %(message)s'))
+logger.addHandler(console_handler)
